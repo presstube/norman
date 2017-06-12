e0ad38c25e1884ecab79e65ec24e777fec7af5df
presstube <j@presstube.com>
2017-06-12 10:37:58 -0400

    small cleanup in anim

--- js/anim.js
+++ js/anim.js
@@ -30,11 +30,6 @@ AFRAME.registerComponent('anim', {
     this.animData = animData
     this.currentFrame = 0
     this.frameChangeTime = null
-    // this.distThresh = 0.001
-    // this.lastPos = null
-    // this.autoNext = false
-    // this.autoPrev = false
-    // this.frameEditing = false
     this.regMarker = new RegMarker(this)
     this.frames = new Frames(this, animData)
     this.homeFrameGhost = new HomeFrameGhost(this)
@@ -113,25 +108,6 @@ AFRAME.registerComponent('anim', {
       if (e.code == 'Enter') {}
       else if (e.code == 'Comma') {this.gotoPrevFrame()}
       else if (e.code == 'Period') {this.gotoNextFrame()}
-
-      else if (e.code == 'BracketLeft' && e.shiftKey) {this.removeFrame()}
-
-      else if (e.code == 'BracketLeft') {this.insertFrameAt('before')}
-      else if (e.code == 'BracketRight') {this.insertFrameAt('after')}
-       
-      // // else if (e.key == 'S') {
-      // //   // console.log('saving: ')
-      // //   uploadAnimData(null, {data: this.animData})
-      // // }
-      // else if (e.key == 'ArrowLeft' && e.altKey && e.shiftKey) {this.fileLoadPrev(!e.ctrlKey)}
-      // else if (e.key == 'ArrowRight' && e.altKey && e.shiftKey) {this.fileLoadNext(!e.ctrlKey)}
-      // else if (e.key == 'ArrowDown' && e.altKey && e.shiftKey && !e.ctrlKey) {this.fileSave()}
-      // else if (e.key == 'ArrowDown' && e.altKey && e.shiftKey && e.ctrlKey) {this.fileSave(false)}
-      // else if (e.code == 'KeyX' && e.altKey) {this.fileDelete()}
-      // else if (e.key == 'o') {this.toggleOnion()}
-      // else if (e.key == ',') {this.changeFPS(-1)}
-      // else if (e.key == '.') {this.changeFPS(1)}
-      // else if (e.key == 't') {this.addLineData([{x:0, y:1, z:2},{x:0, y:1, z:2}], 2)}
     }) 
   },
 
@@ -142,17 +118,7 @@ AFRAME.registerComponent('anim', {
 
     this.pen = primaryHand.object3D
 
-
-    // primaryHand.addEventListener('triggerdown', () => this.handlePrimaryTriggerDown())
-    // primaryHand.addEventListener('triggerup', () => this.handlePrimaryTriggerUp()) 
     primaryHand.addEventListener('lowerbuttondown', () => this.handlePrimaryLowerButtonDown()) 
-    // secondaryHand.addEventListener('triggerdown', () => this.handleSecondaryTriggerDown())
-    // secondaryHand.addEventListener('triggerup', () => this.handleSecondaryTriggerUp())
-    // secondaryHand.addEventListener('LEFT_ON', () => this.handleSecondaryLeftOn())
-    // secondaryHand.addEventListener('RIGHT_ON', () => this.handleSecondaryRightOn())
-    // secondaryHand.addEventListener('LEFT_OFF', () => this.handleSecondaryLeftOff())
-    // secondaryHand.addEventListener('RIGHT_OFF', () => this.handleSecondaryRightOff())
-    // secondaryHand.addEventListener('thumbstickdown', () => this.handleSecondaryThumbstickDown())
   },
 
   // MODIFIERS
@@ -256,72 +222,9 @@ AFRAME.registerComponent('anim', {
     this.insertFrame(frameIndex)
   },
 
-  // startDrawing() {
-  //   if (!this.isDrawing) {
-  //     this.lastPos = this.getLocalPenPos(this.pen.position)
-  //     this.isDrawing = true
-  //     this.startLine(this.lastPos)
-  //   }
-  // },
-
-  // stopDrawing() {
-  //   if (this.isDrawing) {
-  //     this.isDrawing = false
-  //     this.finishLine(this.getLocalPenPos(this.pen.position))
-  //     if (this.autoNext) this.gotoNextFrame()
-  //     if (this.autoPrev) this.gotoPrevFrame()
-  //   }
-  // },
-
-  // handlePrimaryTriggerDown() {
-  //   this.startDrawing()
-  // },
-
-  // handlePrimaryTriggerUp() {
-  //   this.stopDrawing()
-  // },
-
   handlePrimaryLowerButtonDown() {
     this.toggleOnion()
   },
-
-  // handleSecondaryTriggerDown() {
-  //   this.frameEditing = true
-  // },
-
-  // handleSecondaryTriggerUp() {
-  //   this.frameEditing = false
-  // },
-
-  // handleSecondaryLeftOn() {
-  //   this.autoPrev = true
-  //   if (this.frameEditing) {
-  //     this.insertFrameAt('before')
-  //   } else {
-  //     this.gotoPrevFrame()
-  //   }
-  // },
- 
-  // handleSecondaryLeftOff() {
-  //   this.autoPrev = false
-  // },
-
-  // handleSecondaryRightOn() {
-  //   this.autoNext = true
-  //   if (this.frameEditing) {
-  //     this.insertFrameAt('after')
-  //   } else {
-  //     this.gotoNextFrame()
-  //   }
-  // },
-
-  // handleSecondaryRightOff() {
-  //   this.autoNext = false
-  // },
-
-  // handleSecondaryThumbstickDown() {
-  //   if (this.frameEditing) this.removeFrame()
-  // },
 
   // HELPERS
 
