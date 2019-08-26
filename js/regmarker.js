d38ce7df94060eddc4fa1d814ae1cbf7afd46746
presstube <j@presstube.com>
2019-08-26 07:42:01 -0700

    messing with regmarker - progress but currently broken

--- js/regmarker.js
+++ js/regmarker.js
@@ -13,20 +13,58 @@ export default (target, color = 'red', length = 0.005) => {
           opacity: 0.5,
         })
 
-  geometry.vertices = [
-    {x: 0, y: 0, z: 0},
-    {x: length, y: 0, z: 0},
-    {x: 0, y: 0, z: 0},
-    {x: -length, y: 0, z: 0},
-    {x: 0, y: 0, z: 0},
-    {x: 0, y: length, z: 0},
-    {x: 0, y: 0, z: 0},
-    {x: 0, y: -length, z: 0},
-    {x: 0, y: 0, z: 0},
-    {x: 0, y: 0, z: length},
-    {x: 0, y: 0, z: 0},
-    {x: 0, y: 0, z: -length},
-  ]
+
+// spew them random FOO
+
+// change the zero to a var and just run a loop. You can do itttt!
+
+//
+
+
+  function makeVerts() {
+    return _.flatten(_.times(30, () => {
+      let position = {
+        x: _.random(1.0, true),
+        y: _.random(1.0, true),
+        z: _.random(1.0, true)
+      }
+      return [
+        {x: position.x, y: position.y, z: position.z},
+        {x: length, y: position.y, z: position.z},
+        {x: position.x, y: position.y, z: position.z},
+        {x: -length, y: position.y, z: position.z},
+        {x: position.x, y: position.y, z: position.z},
+        {x: position.x, y: length, z: position.z},
+        {x: position.x, y: position.y, z: position.z},
+        {x: position.x, y: -length, z: position.z},
+        {x: position.x, y: position.y, z: position.z},
+        {x: position.x, y: position.y, z: length},
+        {x: position.x, y: position.y, z: position.z},
+        {x: position.x, y: position.y, z: -length},
+      ]
+
+    }))
+  }
+
+
+// console.log('sjsjsj: ', makeVerts())
+  geometry.vertices = makeVerts()
+      console.log('HELOAOOO: ', geometry.vertices)
+
+  // geometry.vertices = [
+  //   {x: 0, y: 0, z: 0},
+  //   {x: length, y: 0, z: 0},
+  //   {x: 0, y: 0, z: 0},
+  //   {x: -length, y: 0, z: 0},
+  //   {x: 0, y: 0, z: 0},
+  //   {x: 0, y: length, z: 0},
+  //   {x: 0, y: 0, z: 0},
+  //   {x: 0, y: -length, z: 0},
+  //   {x: 0, y: 0, z: 0},
+  //   {x: 0, y: 0, z: length},
+  //   {x: 0, y: 0, z: 0},
+  //   {x: 0, y: 0, z: -length},
+  // ]
 
   const mesh = new THREE.LineSegments(geometry, material)
   target.add(mesh)
